4f19364d41245dd972f651c350d01c0ce3adaaff
ZicoRazzi <zicorazzi@gmail.com>
2022-03-07 14:46:54 +0100

    development branch set up

--- netflix-clone-web/src/App.js
+++ netflix-clone-web/src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
+
       <h1>hello from Netflix</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ netflix-clone-web/src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.scss'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>This is NavBar</h1>
+    </div>
+  )
+}
