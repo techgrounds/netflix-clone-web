def30e5eef8e5b51a7fa043de663f962baf31a37
ZicoRazzi <zicorazzi@gmail.com>
2022-03-09 11:42:06 +0100

    button red

--- src/components/LandingHeader/LandingHeader.js
+++ src/components/LandingHeader/LandingHeader.js
@@ -7,7 +7,7 @@ export default function LandingHeader() {
   return (
     <div className="landing-page-header">
       <div className="logo">
-        LOGO
+        
       </div>
       <ButtonRed btnLink='/SignIn' btnTxt='Sign In'/>
     </div>
