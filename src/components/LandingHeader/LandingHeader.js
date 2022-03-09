c2de61dc6360176a7e6cbe59cd40ae0cc5cbb906
ZicoRazzi <zicorazzi@gmail.com>
2022-03-09 11:59:22 +0100

    update header

--- src/components/LandingHeader/LandingHeader.js
+++ src/components/LandingHeader/LandingHeader.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ButtonRed from '../ButtonRed/ButtonRed'
 
+
 import './styles.scss'
 
 export default function LandingHeader() {
   return (
     <div className="landing-page-header">
       <div className="logo">
-        
+      
       </div>
       <ButtonRed btnLink='/SignIn' btnTxt='Sign In'/>
     </div>
