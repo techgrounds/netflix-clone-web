1777a804c43c9c192be9d7607d5abb41f6e6981a
Michael Aular <michaelaular@gmail.com>
2022-03-09 10:48:00 +0100

    lane update

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Lane.scss";
 
-export const  LaneItem = ({ children, width }) => {
+
+export const  LaneItem = ({ children }) => {
     return (
-        <div className="lane-item" style={{ width : width}}>
+        <div className="lane-item">
             {children}
             </div>
     )
 }
 
 const Lane = ({ children }) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = 0
+        } else if (newIndex >= React.Children.count(children)) {
+            newIndex = React.Children.count(children) - 1;
+        }
+        setActiveIndex(newIndex);
+    }
+
+
     return (
         <div className="lane">
-            <div className="inner" style={{ transform: "translateX(-0%)"}}>
+            <div className="inner" style={{ transform: `translateX(-${activeIndex * 15}vw)`}}>
                 {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, { width: "40%" })
+                    return React.cloneElement( child )
                 })}
+            </div>
+            <div className="indicators">
+                <button onClick={() => {updateIndex(activeIndex - 6)}}>PREV</button>
+                <button onClick={() => {updateIndex(activeIndex + 6)}}>NEXT</button>
             </div>
         </div>
     )
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,30 @@
+import Lane, { LaneItem } from "../components/Lane/Lane";
+
 const LandingPage = () => {
   return (
     <>
       <div>Landing Page</div>
+      <Lane>
+        <LaneItem>1</LaneItem>
+        <LaneItem>2</LaneItem>
+        <LaneItem>3</LaneItem>
+        <LaneItem>4</LaneItem>
+        <LaneItem>5</LaneItem>
+        <LaneItem>6</LaneItem>
+        <LaneItem>7</LaneItem>
+        <LaneItem>8</LaneItem>
+        <LaneItem>9</LaneItem>
+        <LaneItem>10</LaneItem>
+        <LaneItem>11</LaneItem>
+        <LaneItem>12</LaneItem>
+        <LaneItem>13</LaneItem>
+        <LaneItem>14</LaneItem>
+        <LaneItem>15</LaneItem>
+        <LaneItem>16</LaneItem>
+        <LaneItem>17</LaneItem>
+        <LaneItem>18</LaneItem>
+        <LaneItem>19</LaneItem>
+      </Lane>
     </>
   );
 };
