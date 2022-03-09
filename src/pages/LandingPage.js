fa0e44c3ddd84b78d3720f47c2818dee87260eb3
ZicoRazzi <zicorazzi@gmail.com>
2022-03-09 14:57:12 +0100

    landing page update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,18 +1,10 @@
 import LandingHero from './../components/LandingHero/LandingHero';
 
-
 const LandingPage = () => {
   return (
-<<<<<<< HEAD
     <div className="landingPage">
       <LandingHero />
-      
     </div>
-=======
-    <>
-      <div>Landing Page</div>
-      </>
->>>>>>> d0d592b72313b2630a3f417146db9da8e456e87a
   );
 };
 
