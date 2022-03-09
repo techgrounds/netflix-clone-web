805801c5157d08ba21e6625017069f3a71965907
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-03-09 10:52:49 +0100

    first icons

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,24 @@
-
+import { IconDetail } from "../components/Icons/IconDetail";
+import { NetflixLogo } from "../components/Logos/NetflixLogo";
+import { IconArrowLeft } from "../components/Icons/IconArrowLeft";
+import { IconArrowDown } from "../components/Icons/IconArrowDown";
+import { IconNotification } from '../components/Icons/IconNotification';
+import { ButtonRoundDark } from "../components/ButtonRound/ButtonRound";
 const LandingPage = () => {
   return (
     <>
       <div>Landing Page</div>
+      <IconDetail />
+      <ButtonRoundDark>
+        <IconArrowLeft />
+      </ButtonRoundDark>
+      <ButtonRoundDark>
+        <IconArrowDown />
+      </ButtonRoundDark>
+      <ButtonRoundDark>
+        <IconNotification />
+      </ButtonRoundDark>
+      <NetflixLogo />
     </>
   );
 };
