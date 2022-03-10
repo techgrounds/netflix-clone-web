b9f240d473ada0633b859711cc030b2bfc4381a6
ZicoRazzi <zicorazzi@gmail.com>
2022-03-10 15:08:56 +0100

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-import "./assets/scss/main.scss";
+import './assets/scss/main.scss';
 
-import Navbar from "./components/Navbar/Navbar";
-import ScrollToTop from "./components/ScrollToTop";
-import LandingPage from "./pages/LandingPage";
-import HomePage from "./pages/HomePage";
-import GenrePage from "./pages/GenrePage";
-import ErrorPage from "./pages/ErrorPage";
-import SearchPage from "./pages/SearchPage";
-import FooterLandingPage from "./components/FooterLandingPage/FooterLandingPage";
+import Navbar from './components/Navbar/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+import LandingPage from './pages/LandingPage';
+import HomePage from './pages/HomePage';
+import GenrePage from './pages/GenrePage';
+import ErrorPage from './pages/ErrorPage';
+import SearchPage from './pages/SearchPage';
+import FooterLandingPage from './components/FooterLandingPage/FooterLandingPage';
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
             <Route exact path="*" element={<ErrorPage />} />
           </Routes>
         </ScrollToTop>
-        <FooterLandingPage />
+        {/* <FooterLandingPage /> */}
       </Router>
     </div>
   );
