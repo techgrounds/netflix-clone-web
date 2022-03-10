0b9486fa782fdc2194576a2b3948b500945b35d3
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-03-10 16:09:14 +0100

    footer english

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,10 @@
-import LandingHero from './../components/LandingHero/LandingHero';
-
+import LandingHero from "./../components/LandingHero/LandingHero";
+import FooterLandingPage from "../components/FooterLandingPage/FooterLandingPage";
 const LandingPage = () => {
   return (
     <div className="landingPage">
       <LandingHero />
+      <FooterLandingPage />
     </div>
   );
 };
