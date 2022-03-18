ddb8be6fbbbd8ba14de2503e8f7acb01be65cd81
ZicoRazzi <zicorazzi@gmail.com>
2022-03-18 13:21:41 +0100

    git issue fixed

---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './styles.scss'
+
+
+
+export default function ({ handleChange, label, ...otherProps }) {
+  return (
+    <>
+      {label && <label>{label}</label>}
+
+      <input className="input-field" onChange={handleChange} {...otherProps} />
+    </>
+  );
+}
