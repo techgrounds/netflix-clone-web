d671a6f31552dc812d779fbf3c9133e3852a4f25
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-03-25 16:33:21 +0100

    update modal styling

--- src/components/MiniModal/MiniModalDetails/MiniModalDetails.js
+++ src/components/MiniModal/MiniModalDetails/MiniModalDetails.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import "./MiniModalDetails.scss"
+import React from "react";
+import "./MiniModalDetails.scss";
 
-import { IconAdd } from '../../Icons/IconAdd'
-import { IconLike } from '../../Icons/IconLike'
-import { IconPlayBlack } from '../../Icons/IconPlayBlack'
+import { IconAdd } from "../../Icons/IconAdd";
+import { IconLike } from "../../Icons/IconLike";
+import { IconDisLike } from "../../Icons/IconDisLike";
+import { IconPlayBlack } from "../../Icons/IconPlayBlack";
+import { IconArrowDown } from "../../Icons/IconArrowDown";
 
 const MiniModalDetails = () => {
   return (
     <>
-          <div className="minimodal-button-wrapper">
+      <div className="minimodal-button-wrapper">
         <div className="left-content">
-          <button className="play-button"><IconPlayBlack/></button>
-          <button className="add-button"><IconAdd/></button>
-          <button className="like-button"><IconLike/></button>
-
-          
+          <button className="play-button">
+            <IconPlayBlack />
+          </button>
+          <button className="add-button">
+            <IconAdd />
+          </button>
+          <button className="like-button">
+            <IconLike />
+            <div className="rating-button-wrapper">
+              <button className="dislike-button rating-button">
+                <IconDisLike />
+              </button>
+              <button className="like-button rating-button">
+                <IconLike />
+              </button>
+              <button className="lovelike-button rating-button">
+                <IconLike />
+              </button>
+            </div>
+          </button>
         </div>
         <div className="right-content">
-          <button className="moreInfo-button">▼</button>
+          <button className="moreInfo-button">
+            <IconArrowDown />
+          </button>
         </div>
       </div>
       <div className="info-container">
@@ -28,12 +47,12 @@ const MiniModalDetails = () => {
         <span className="feature-badge">HD</span>
       </div>
       <div className="tag-container">
-        <span className="tag-item">Witty</span>•
-        <span className="tag-item">Feel-Good</span>•
-        <span className="tag-item">Exciting</span>
+        <span className="tag-item-mini">Witty</span>•
+        <span className="tag-item-mini">Feel-Good</span>•
+        <span className="tag-item-mini">Exciting</span>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MiniModalDetails
+export default MiniModalDetails;
