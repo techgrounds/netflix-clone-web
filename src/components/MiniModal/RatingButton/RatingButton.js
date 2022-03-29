20b526bd5299557f771e53c59367899cf51b6a09
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-03-29 13:26:11 +0200

    Ratingbutton hover

---
+++ src/components/MiniModal/RatingButton/RatingButton.js
@@ -0,0 +1,62 @@
+
+import "./RatingButton.scss";
+import ReactTooltip from "react-tooltip";
+
+import { IconLike } from "../../Icons/IconLike";
+import { IconDisLike } from "../../Icons/IconDisLike";
+
+const RatingButton = () => {
+  return (
+    <>
+      <button className="like-button like-icon-wrapper">
+        <div className="like-button-icon">
+          <IconLike />
+        </div>
+        <div className="rating-button-wrapper">
+          <button
+            className="dislike-button rating-button"
+            data-tip
+            data-for="dislike"
+            tabIndex="0"
+          >
+            <IconDisLike />
+          </button>
+          <ReactTooltip
+            id="dislike"
+            className="tool-tip"
+            place="top"
+            type="light"
+          >
+            Not For Me
+          </ReactTooltip>
+          <button
+            className="like-button rating-button"
+            data-tip
+            data-for="like"
+            tabIndex="0"
+
+          >
+            <IconLike />
+          </button>
+          <ReactTooltip id="like" className="tool-tip" place="top" type="light">
+            I Like This
+          </ReactTooltip>
+          <button
+            className="lovelike-button rating-button"
+            data-tip
+            data-for="love"
+            tabIndex="0"
+
+          >
+            <IconLike />
+          </button>
+          <ReactTooltip id="love" className="tool-tip" place="top" type="light">
+            Love This!
+          </ReactTooltip>
+        </div>
+      </button>
+    </>
+  );
+};
+
+export default RatingButton;
