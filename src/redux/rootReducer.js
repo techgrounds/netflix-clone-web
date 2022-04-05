d46ff6426ea53baa16044cf84c8d5c7f104cd876
Zico Razzi <zicorazzi@gmail.com>
2022-04-05 20:01:14 +0200

    root reducer update

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
+import moviesReducer from './movies/movies.reducer';
 import searchReducer from './search/search.reducer';
 
 const persistConfig = {
@@ -8,6 +9,9 @@ const persistConfig = {
   storage,
 };
 
-const rootReducer = combineReducers({ search: searchReducer });
+const rootReducer = combineReducers({
+  search: searchReducer,
+  movies: moviesReducer,
+});
 
 export default persistReducer(persistConfig, rootReducer);
