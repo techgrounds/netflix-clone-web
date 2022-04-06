3ff8b8d3dee2a49d0e432bcd149f9d164d97550a
RDOToole89 <roibinotoole@gmail.com>
2022-04-06 14:34:46 +0200

    refactor movie data transformation to movies reducer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,28 +11,18 @@ import { v4 as uuidv4 } from 'uuid';
 const HomePage = () => {
   const dispatch = useDispatch();
   const allMoviesSelector = useSelector((state) => state.movies.allMovies);
-  const allMovies = allMoviesSelector?.map((movieSet) => {
-    return movieSet.results
-      .map((movie) =>
-        !movie.backdrop_path
-          ? undefined
-          : {
-              key: uuidv4(),
-              title: movie.title,
-              id: movie.backdrop_path,
-            }
-      )
-      .filter((x) => x);
-  });
 
   useEffect(() => {
     dispatch(fetchMoviesResultsAsync());
   }, []);
+
   return (
     <>
       <Navbar />
       <HomeHero />
-      {allMovies?.map((movieSet) => {
+      {allMoviesSelector?.map((movieSet) => {
+        // console.log('SET', movieSet);
+
         return (
           <Lane laneTitle={'lane title'} movies={movieSet} key={uuidv4()} />
         );
--- src/redux/movies/movies.actions.js
+++ src/redux/movies/movies.actions.js
@@ -1,6 +1,7 @@
 import { moviesActionTypes } from './movies.types';
 import requests from '../../requests';
 import axios from '../../axiosInstance';
+import { v4 as uuidv4 } from 'uuid';
 
 export const fetchMoviesResultsRequest = () => ({
   type: moviesActionTypes.FETCH_MOVIES_RESULTS_REQUEST,
@@ -28,16 +29,28 @@ export const fetchMoviesResultsAsync = () => {
     try {
       const request = await axios.get(requests.fetchDiscover);
 
-      const allMoviesExcluding = request.data.filter((x, i) =>
-        i ? true : false
-      );
+      const allMoviesExcluding = request.data
+        .filter((x, i) => (i ? true : false))
+        .map((movieSet) => {
+          return movieSet.results
+            .map((movie) =>
+              !movie.backdrop_path
+                ? undefined
+                : {
+                    key: uuidv4(),
+                    title: movie.title,
+                    id: movie.backdrop_path,
+                  }
+            )
+            .filter((x) => x);
+        });
 
       dispatch(fetchMoviesResultsSuccess(allMoviesExcluding));
       // console.log('ALL MOVIES EX', allMoviesExcluding);
       const randomMovieSet =
         Math.floor(Math.random() * (allMoviesExcluding.length - 1)) + 1;
       // console.log('RANDOM MOVIE', randomMovieSet);
-      const movies = allMoviesExcluding[randomMovieSet].results;
+      const movies = allMoviesExcluding[randomMovieSet];
 
       const selectRandomMovie = Math.floor(Math.random() * movies.length - 1);
 
