e64c35da496801f01bed4ca1630c7d5e31abb9a2
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-04-12 12:11:31 +0200

    iconlist icongallery

---
+++ src/components/Icons/IconGallery.js
@@ -0,0 +1,20 @@
+export const IconGallery = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        role="img"
+        width="2.1em"
+        height="2.1em"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 25 25"
+      >
+        <path
+          fill="currentColor"
+          d="M19 19h-6v-6h6v6Zm-8 0H5v-6h6v6Zm8-8h-6V5h6v6Zm-8 0H5V5h6v6Z"
+        />
+      </svg>
+    </>
+  );
+};
