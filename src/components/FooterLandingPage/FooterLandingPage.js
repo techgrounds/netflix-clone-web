9ee2f7041fb04063e6ead561d227091ce3ed0d97
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-04-13 11:07:29 +0200

    footer landingpage translation

--- src/components/FooterLandingPage/FooterLandingPage.js
+++ src/components/FooterLandingPage/FooterLandingPage.js
@@ -1,102 +1,107 @@
 import React from "react";
 import "./styles.scss";
-import { IconWorld } from "../Icons/IconWorld";
-//import { IconCaretDown } from "../Icons/IconCaretDown";
 import LanguagesSelector from "../LanguagesSelector/LanguagesSelector";
 
+import { useContext } from "react";
+import { LangContext } from "../../redux/languages/languages.context";
+
 function FooterLandingPage() {
+  const { language } = useContext(LangContext);
+
   return (
-    // <div className="card-container">
-      <footer className="footer-landingpage-wrapper">
-        <div className="footer-content">
-          <p className="footer-top">
-            Questions? Call
-            <a className="footer-top-call" href="">
-              0800-022-9647
+    <footer className="footer-landingpage-wrapper">
+      <div className="footer-content">
+        <p className="footer-top">
+          {language === "EN" ? "Questions? Call" : "Vragen? Bel"}
+          <a className="footer-top-call" href="">
+            0800-022-9647
+          </a>
+        </p>
+        <ul className="box-footer-links">
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "FAQ" : "Veelgestelde vragen"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "Help Centre" : "Helpcentrum"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "Account" : "Account"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "Media Centre" : "Mediacenter"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN"
+                ? "Investor Relations"
+                : "Relaties met investeerders"}
             </a>
-          </p>
-          <ul className="box-footer-links">
-            <li className="item-footer-link">
-              <a href="">FAQ</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Help Centre</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Account</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Media Centre</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Investor Relations</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Jobs</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Redeem gift cards</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Buy gift cards</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Ways to Watch</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Terms of Use</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Privacy</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Cookie Preferences</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Corporate Information</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Contact Us</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Speed Test</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Legal Notices</a>
-            </li>
-            <li className="item-footer-link">
-              <a href="">Only on Netflix</a>
-            </li>
-          </ul>
-          <div className="footerLanguageSelect">
-            <LanguagesSelector />
-          </div>
-          {/*
-          <div className="container-select-lang" id="lang-switcher">
-            <div data-ui-a className="container-ui">
-              <label
-                htmlFor="lang-switcher-select"
-                className="ui-label"
-              ></label>
-            <div className="selectArrowGlobe-footerlandingpage">
-                <IconWorld />
-                <select
-                  className="ui-select"
-                  name="Nederlands"
-                  placeholder="language-switcher"
-                  id=""
-                >
-                  <option value="">English</option>
-                  <option value="">Nederlands</option>
-                </select>
-              </div>
-            </div>
-          </div>
-  */}    
-          <p>Neflix Netherlands</p>
+          </li>
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "Jobs" : "Vacatures"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN"
+                ? "Redeem gift cards"
+                : "Cadeaubonnen gebruiken"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Buy gift cards" : "Cadeaubonnen kopen"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN"
+                ? "Ways to Watch"
+                : "Hoe kun je Netflix kijken?"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Terms of Use" : "Gebruiksvoorwaarden"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "Privacy" : "Privacy"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Cookie Preferences" : "Cookievoorkeuren"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Corporate Information" : "Bedrijfsgegevens"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Contact Us" : "Contact opnemen"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">{language === "EN" ? "Speed Test" : "Snelheidstest"}</a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Legal Notices" : "Wettelijke bepalingen"}
+            </a>
+          </li>
+          <li className="item-footer-link">
+            <a href="">
+              {language === "EN" ? "Only on Netflix" : "Alleen op Netflix"}
+            </a>
+          </li>
+        </ul>
+        <div className="footerLanguageSelect">
+          <LanguagesSelector />
         </div>
-      </footer>
-    // </div>
+        <p>{language === "EN" ? "Neflix Netherlands" : "Netflix Nederland"}</p>
+      </div>
+    </footer>
   );
 }
 
