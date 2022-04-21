16c41fc4a851d7db9aa3572e8d06ed46670ce3a7
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-04-21 11:21:41 +0200

    languagereducer change state

--- src/redux/languages/languages.reducer.js
+++ src/redux/languages/languages.reducer.js
@@ -5,7 +5,7 @@ const initialLanguage = "EN";
 export const languagesReducer = (state = initialLanguage, action) => {
   switch (action.type) {
     case actions.changeLanguage:
-      return { ...state, language: action.payload };
+      return { language: action.payload };
     default:
       return state;
   }
