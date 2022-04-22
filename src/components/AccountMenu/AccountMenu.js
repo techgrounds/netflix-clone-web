cc0f4cdd1c0cf078620d5b8869493d21c5c090d4
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2022-04-22 12:57:08 +0200

    account change

---
+++ src/components/AccountMenu/AccountMenu.js
@@ -0,0 +1,45 @@
+//import { NavLink } from "react-router-dom"
+//import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { changeAccount } from "../../redux/account/account.action"
+import { useContext, useEffect } from "react"
+import { AccountContext } from "../../redux/account/account.context"
+
+const accountData = require("../../assets/accountData.json")
+
+const AccountMenu = () => {
+  const dispatch = useDispatch()
+  console.log(accountData)
+  const { account, setAccount } = useContext(AccountContext)
+  console.log(account)
+  
+  
+
+  const customAccount = accountData.map((profile) => {
+      console.log(profile.name)
+    return (
+      <li
+        className="sub-menu-item profile"
+        key={profile.name}
+        value={profile.name}
+        onClick={() => dispatch(changeAccount(setAccount(profile.name)))}
+      >
+        <div>
+          <div className="profile-link custom-account">
+            <div className="avatar-wrapper">
+              <img
+                className="profile-icon custom-profile-icon"
+                src={`../../assets/images/accounts/${profile.img}`}
+                alt={profile.name}
+              />
+            </div>
+            <span className="profile-name">{profile.name}</span>
+          </div>
+        </div>
+      </li>
+    )
+  })
+  return <>{customAccount}</>
+}
+
+export default AccountMenu
