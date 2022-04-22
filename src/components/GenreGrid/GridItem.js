80833127d48248a0ded52377a3bdd58e83838377
Janou <janoutreffers@hotmail.com>
2022-04-22 10:22:55 +0200

    add genre grid

--- src/components/GenreGrid/GridItem.js
+++ src/components/GenreGrid/GridItem.js
@@ -1,93 +1,93 @@
-import useWindowSize from '../Lane/WindowSize'
-import { useState, useEffect } from 'react'
-import MiniModal from '../MiniModal/MiniModal'
-import './GenreGrid.scss'
+import useWindowSize from "../Lane/WindowSize";
+import { useState, useEffect } from "react";
+import MiniModal from "../MiniModal/MiniModal";
+import "./GenreGrid.scss";
 
 export const GridItem = ({
-    movie,
-    leftIndex,
-    rightIndex,
-    index,
-    // mute,
-    // setMute,
+  movie,
+  leftIndex,
+  rightIndex,
+  index
 }) => {
-    const size = useWindowSize()
-    const [loadMovie, setLoadMovie] = useState(false)
-    const [hovered, setHovered] = useState(false)
-    const sleep = (milliseconds) => {
-        return new Promise((resolve) => setTimeout(resolve, milliseconds))
+  const size = useWindowSize();
+  const [loadMovie, setLoadMovie] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
+
+  useEffect(() => {
+    if (hovered) {
+      setLoadMovie(true);
     }
+    if (!hovered) {
+      setLoadMovie(false);
+    }
+  }, [hovered]);
 
-    useEffect(() => {
-        if (hovered) {
-            
-            setLoadMovie(true)
-        }
-        if (!hovered) {
-           
-            setLoadMovie(false)
-        }
-    }, [hovered])
-
-    return (
+  return (
+    <div
+      className="gridItem"
+      style={{ height: `${size.itemHeight}vw`, width: `${size.itemWidth}vw` }}
+      onMouseEnter={() => {
+        setHovered(true);
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+      }}
+    >
+      {loadMovie && (
         <div
-            className='gridItem'
-            style={{ height: `${size.itemHeight}vw`, width: `${size.itemWidth}vw` }}
-            onMouseEnter={() => {
-                setHovered(true)
-            }}
-            onMouseLeave={() => {
-                setHovered(false)
-            }}>
-            {loadMovie && (
-                <div
-                    className={`miniModal
-                 ${index === leftIndex
-                            ? 'leftModal'
-                            : index === rightIndex
-                                ? 'rightModal'
-                                : 'not'
-                        }
+          className={`miniModal
+                 ${
+                   index === leftIndex
+                     ? "leftModal"
+                     : index === rightIndex
+                     ? "rightModal"
+                     : "not"
+                 }
                  `}
-                    style={{
-                        height: `${size.itemHeight * 2.5}vw`,
-                        width: `${size.itemWidth * 1.5}vw`,
-                    }}>
-                    {loadMovie && (
-                        <MiniModal
-                            loadMovie={loadMovie}
-                            moviePoster={movie.image}
-                            movieTitle={movie.title}
-                            setLoadMovie={setLoadMovie}
-                            trailer={movie.trailer}
-                            runtime={movie.runtime}
-                            rating={movie.rating}
-                            movie={movie}
-                            // mute={mute}
-                            // setMute={setMute}
-                        />
-                    )}
-                </div>
-            )}
-
-            <img
-                src={movie.image}
-                alt={movie.title}
-                className='movie-image'
-                style={{
-                    width: '0',
-                    height: '0',
-                    objectFit: 'contain',
-                }}
+          style={{
+            height: `${size.itemHeight * 2.5}vw`,
+            width: `${size.itemWidth * 1.5}vw`,
+          }}
+        >
+          {loadMovie && (
+            <MiniModal
+              loadMovie={loadMovie}
+              moviePoster={movie.image}
+              movieTitle={movie.title}
+              setLoadMovie={setLoadMovie}
+              trailer={movie.trailer}
+              runtime={movie.runtime}
+              rating={movie.rating}
+              movie={movie}
+              // mute={mute}
+              // setMute={setMute}
             />
-            {/* <div
-                className='laneItemTitle'
-                style={{ width: `${size.itemWidth * 0.9}vw`, overflow: `hidden` }}>
-                {movie.title}
-                <div className='movieLogo'>
-                    <img src={movie.logo} />
-                </div>
-            </div> */}
+          )}
         </div>
-    )
-}
+      )}
+
+      <img
+        src={movie.image}
+        alt={movie.title}
+        className="movie-image"
+        style={{
+          width: "0",
+          height: "0",
+          objectFit: "contain",
+        }}
+      />
+      {/* <div
+        className="laneItemTitle"
+        style={{ width: `${size.itemWidth * 0.9}vw`, overflow: `hidden` }}
+      >
+        {movie.title}
+        <div className="movieLogo">
+          <img src={movie.logo} />
+        </div>
+      </div> */}
+    </div>
+  );
+};
