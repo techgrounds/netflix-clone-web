06f39720d3ccfd1d05585b76c4ffe898c87dc888
Zico Razzi <zicorazzi@gmail.com>
2022-04-28 12:28:22 +0200

    genre page update

--- src/pages/GenrePage.js
+++ src/pages/GenrePage.js
@@ -1,60 +1,49 @@
-import React, { useEffect, useState } from "react";
-import HomeHero from "../components/HomeHero/HomeHero";
-import FooterBrowserPage from "../components/FooterBrowserPage/FooterBrowserPage";
-import Lane from "../components/Lane/Lane";
-import GenreGrid  from "../components/GenreGrid/GenreGrid"
-import { useDispatch, useSelector } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
-import { fetchGenresResultsAsync } from "../redux/genres/genres.actions";
-import { GenreGrid } from "../components/GenreGrid/GenreGrid";
-import { genreGridActive } from "../redux/genres/genres.actions";
-
+import React, { useEffect, useState } from 'react';
+import HomeHero from '../components/HomeHero/HomeHero';
+import FooterBrowserPage from '../components/FooterBrowserPage/FooterBrowserPage';
+import Lane from '../components/Lane/Lane';
+import GenreGrid from '../components/GenreGrid/GenreGrid';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMoviesResultsAsync } from '../redux/movies/movies.actions';
+import { v4 as uuidv4 } from 'uuid';
 const GenrePage = () => {
   const [isVideoPlaying, setIsVideoPlaying] = useState(true);
   const gridActive = useSelector((state) => state.genres.gridActive);
   const moviesByGenreData = useSelector((state) => state.genres.allGenres);
   const selectCurrentGenre = useSelector((state) => state.genres.selectGenre);
-  const selectMovieSet = moviesByGenreData?.find((movieSet) => movieSet.genre === selectCurrentGenre);
+  const selectMovieSet = moviesByGenreData?.find(
+    (movieSet) => movieSet.genre === selectCurrentGenre
+  );
   const dispatch = useDispatch();
-  const selectCurrentGenre = useSelector((state) => state.genres.selectGenre);
-  const gridActive = useSelector((state) => state.genres.gridActive);
-  const moviesByGenreData = useSelector((state) => state.genres.allGenres);
-  const movieData = useSelector((state) => state.movies.movie);
-
-  console.log("moviesByGenreData ", moviesByGenreData);
-  console.log("movieData :", movieData);
-
   useEffect(() => {
-    dispatch(fetchGenresResultsAsync());
-    dispatch(genreGridActive(false));
-
+    dispatch(fetchMoviesResultsAsync());
+  }, [dispatch]);
   return (
     <>
       <HomeHero
         setIsVideoPlaying={setIsVideoPlaying}
         isVideoPlaying={isVideoPlaying}
-        isModalVisible={isModalVisible}
-        setIsModalVisible={setIsModalVisible}
-        movieData={movieData}
       />
-      {gridActive ? selectMovieSet?.movies &&
-      <GenreGrid
-      genreTitle={selectMovieSet.genre}
-      moviesByGenreData={selectMovieSet?.movies}/>
-      :
-      moviesByGenreData?.map((movieSet) => {
-        return (
-        <Lane
-        laneTitle={movieSet.genre}
-        movies={movieSet.movies}
-        trailer={movieSet.trailer}
-        key={uuidv4()}
-        setIsVideoPlaying={setIsVideoPlaying}
-        />);
-      })}
+      {gridActive
+        ? selectMovieSet?.movies && (
+            <GenreGrid
+              genreTitle={selectMovieSet.genre}
+              moviesByGenreData={selectMovieSet?.movies}
+            />
+          )
+        : moviesByGenreData?.map((movieSet) => {
+            return (
+              <Lane
+                laneTitle={movieSet.genre}
+                movies={movieSet.movies}
+                trailer={movieSet.trailer}
+                key={uuidv4()}
+                setIsVideoPlaying={setIsVideoPlaying}
+              />
+            );
+          })}
       <FooterBrowserPage />
     </>
   );
 };
-
 export default GenrePage;
